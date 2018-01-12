3de9108e4156781c753d0704191ad24d8930411a
Lucas <lucas.cairesd@gmail.com>
2018-01-11 22:12:59 -0200

    Refactoring the script to use class and ES6 features

--- src/brinput.js
+++ src/brinput.js
@@ -1,89 +1,90 @@
 /**
  * brinput.js
- * @version: v1.0.0
+ * @version: v1.0.1
  * @author: Lucas Caires
  *
  * Plugin utilizado em projetos pessoais para adicionar máscaras em campos de formulário
  */
 
+class brInput {
 
-	var _br,
-    brInput = {
+  //Constructor
+  constructor(config) {
+    this.config = {}
+    this.config.sel = config && config.sel || "[br-input]"
+    this.config.attr = config && config.attr || "br-input"
+    this.init()
+  }
 
-	//Seletor	
-	sel: document.querySelectorAll("[br-input]"),
+  //Init
+  init() {
+    if("object" === typeof this.getElements()) {
+      this.getElements().forEach(each => this.setListener(each))
+    }
+  }
 
-	//Inicia
-	init: function init() {
-		_br = this;
-		_br.bind();
-	},
+  //Get all nodes
+  getElements() {
+    return this.config.sel && document.querySelectorAll(this.config.sel)
+  }
 
-	bind: function bind() {
-		_br.sel.forEach(_br.setListener);
-	},
+  //Listen to the keyboard and fire the relative method
+  setListener(each) {
+    const input = each.getAttribute(this.config.attr)
+    each.addEventListener('keyup', key => {
+      if ("telefone" === input) this.telefoneMask(each)
+      if ("cep" === input) this.cepMask(each)
+      if ("cpf" === input) this.cpfMask(each)
+      if ("cnpj" === input) this.cnpjMask(each)
+    })
+  }
+  
+  //Adiciona o atributo max-length
+  setMaxlen(el, size) {
+    el.setAttribute('maxlength', size)
+  }
 
-	//Bind do teclado
-	setListener: function setListener(each) {
-		var input = each.getAttribute("br-input");
-		each.addEventListener('keyup', function (key) {
-			_br.setMask(input, this);
-		});
-	},
+  //Mascara para Telefone (00) 0000-0000
+  telefoneMask(k) {
+    this.setMaxlen(k, 15)
+    let a = k.value
+    a = a.replace(/\D/g, "")
+    a = a.replace(/^(\d{2})(\d)/g, "($1) $2")
+    if (a.length > 12) a = a.replace(/(\d)(\d{4})$/, "$1-$2")
+    k.value = a
+  }
 
-	//Seta os métodos para seus respectivos atributos
-	setMask: function setMask(input, key) {
-		if ("telefone" === input) _br.telefoneMask(key);
-		if ("cep" === input) _br.cepMask(key);
-		if ("cpf" === input) _br.cpfMask(key);
-		if ("cnpj" === input) _br.cnpjMask(key);
-	},
+  //Mascara para CEP 000000-000
+  cepMask(k) {
+    this.setMaxlen(k, 9)
+    let a = k.value
+    a = a.replace(/\D/g, "")
+    a = a.replace(/^(\d{5})(\d)/g, "$1-$2")
+    k.value = a
+  }
 
-	//Adiciona o atributo max-length
-	setMaxlen: function setMaxlen(el, size) {
-		el.setAttribute('maxlength', size);
-	},
+  //Mascara para CPF 000.000.000-00
+  cpfMask(k) {
+    this.setMaxlen(k, 14)
+    let a = k.value
+    a = a.replace(/\D/g, "")
+    a = a.replace(/(\d{3})(\d)/, "$1.$2")
+    a = a.replace(/(\d{3})(\d)/, "$1.$2")
+    a = a.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    k.value = a
+  }
 
-	//Mascara para Telefone (00) 0000-0000
-	telefoneMask: function telefoneMask(k) {
-		_br.setMaxlen(k, 15);
-		var a = k.value;
-		a = a.replace(/\D/g, "");
-		a = a.replace(/^(\d{2})(\d)/g, "($1) $2");
-		if (a.length > 12) a = a.replace(/(\d)(\d{4})$/, "$1-$2");
-		k.value = a;
-	},
+  //Mascara para CNPJ 00.000.000/0000-00
+  cnpjMask(k) {
+    this.setMaxlen(k, 18)
+    let a = k.value
+    a = a.replace(/\D/g, "")
+    a = a.replace(/(\d{2})(\d)/, "$1.$2")
+    a = a.replace(/(\d{3})(\d)/, "$1.$2")
+    a = a.replace(/(\d{3})(\d)/, "$1/$2")
+    a = a.replace(/(\d{4})(\d)/, "$1-$2")
+    k.value = a
+  }
+}
 
-	//Mascara para CEP 000000-000
-	cepMask: function cepMask(k) {
-		_br.setMaxlen(k, 9);
-		var a = k.value;
-		a = a.replace(/\D/g, "");
-		a = a.replace(/^(\d{5})(\d)/g, "$1-$2");
-		k.value = a;
-	},
-
-	//Mascara para CPF 000.000.000-00
-	cpfMask: function cpfMask(k) {
-		_br.setMaxlen(k, 14);
-		var a = k.value;
-		a = a.replace(/\D/g, "");
-		a = a.replace(/(\d{3})(\d)/, "$1.$2");
-		a = a.replace(/(\d{3})(\d)/, "$1.$2");
-		a = a.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
-		k.value = a;
-	},
-
-	//Mascara para CNPJ 00.000.000/0000-00
-	cnpjMask: function cnpjMask(k) {
-		_br.setMaxlen(k, 18);
-		var a = k.value;
-		a = a.replace(/\D/g, "");
-		a = a.replace(/(\d{2})(\d)/, "$1.$2");
-		a = a.replace(/(\d{3})(\d)/, "$1.$2");
-		a = a.replace(/(\d{3})(\d)/, "$1/$2");
-		a = a.replace(/(\d{4})(\d)/, "$1-$2");
-		k.value = a;
-	}
-};
-brInput.init()
+new brInput()
